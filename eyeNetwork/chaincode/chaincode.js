933c25467760e1ac1b9f6913fdd1cc1da75aaaa1
Jiawei <lostmoonstar@gmail>
2018-11-08 19:53:02 -0500

    update chaincode

--- eyeNetwork/chaincode/chaincode.js
+++ eyeNetwork/chaincode/chaincode.js
@@ -19,15 +19,13 @@
  * 
  */
 
-
-
-
-
-
 const shim = require('fabric-shim');
 const util = require('util');
 
+
 var Chaincode = class {
+
+    
     async Init(stub) {
         console.info('===============Eye network transfer system start================')
         /**
@@ -37,27 +35,83 @@ var Chaincode = class {
 
     }
 
-    async addAdProvider(name, balance) {
-
+    async addAdProvider(stub, name, id, balance) {
+        let newAdProvider={
+            providerName: name,
+            providerId: id,
+            balance: balance
+        }
+        await stub.putState('adProvider '+newAdProvider.providerId, 
+                              Buffer.from(JSON.stringify(newAdProvider)));
+        console.log('adProvider '+newAdProvider.providerId+' has already been added with balance '+newAdProvider.balance);
     }
 
 
-    async updataIssuerBalance(newBalance) {
+    async updateTokenBalance(stub, newBalance) {
+        console.info('=================Start: update token balance=====================');
+        if (newBalance<=0){
+            throw new Error('new balance can not be negative');
+        }
+        let tokenIssuerBytes = await stub.getState('tokenIssuer');
+        let tokenIssuer=JSON.parse(tokenIssuerBytes);
+        tokenIssuer.balance=newBalance;
+        await stub.putState('tokenIssuer',JSON.stringify(tokenIssuer));
+        console.info('====================END: token balance is updated================');
 
     }
 
-    async adProviderTopVideoProvider(adPid, vPid, adId, vId, amount, ) {
+    async adProvider2VideoProvider(stub, adPid, vPid, adId, vId, amount, ) {
+        console.info('=============Transaction happen===============');
+        let adProviderByte= await stub.getState('adProvider '+adPid)
+        let adProvider=JSON.parse(adProviderByte);
+        let videoProviderByte= await stub.getState('videoProvider '+vPid);
+        let videoProvider=JSON.parse(videoProviderByte);
+
+        adProvider.balance=adProvider.balance-amount;
+        videoProvider.balance=videoProvider.balance+amount;
+        await stub.putState('adProvider '+adPid,JSON.stringify(adProvider));
+        await stub.putState('videoProvider '+vPid,JSON.stringify(videoProvider));
+        console.info("transaction is done. ad provider "+adPid+" paid "+amount+" token to video provider "
+                        +vPid+" to attach ad "+adId+ " on video "+vid);
 
     }
-    async tokenIssuerToadProvider() {
+    async tokenIssuer2AdProvider(stub,adPid,amount) {
+        console.info('===================Transaction happen=================');
+        let adProviderByte= await stub.getState('adProvider '+adPid)
+        let adProvider=JSON.parse(adProviderByte);
+        let tokenIssuerBytes = await stub.getState('tokenIssuer');
+        let tokenIssuer=JSON.parse(tokenIssuerBytes);
+
+        tokenIssuer.balance=tokenIssuer.balance-amount;
+        adProvider.balance=adProvider.balance+amount;
+
+        await stub.putState('videoProvider '+vPid,JSON.stringify(videoProvider));
+        await stub.putState('tokenIssuer',JSON.stringify(tokenIssuer));
 
     }
-    async videoToTokenIssuer() {
+    async videoProvider2TokenIssuer(stub,vPid,amount) {
+        console.info('===================Transaction happen=================');
+        let tokenIssuerBytes = await stub.getState('tokenIssuer');
+        let tokenIssuer=JSON.parse(tokenIssuerBytes);
+        let videoProviderByte= await stub.getState('videoProvider '+vPid);
+        let videoProvider=JSON.parse(videoProviderByte);
 
+        tokenIssuer.balance=tokenIssuer.balance+amount;
+        videoProvider.balance=videoProvider.balance-amount;
+
+        await stub.putState('tokenIssuer',JSON.stringify(tokenIssuer));
+        await stub.putState('videoProvider '+vPid,JSON.stringify(videoProvider));
     }
 
-    async addVideoProvider(name, balance) {
-
+    async addVideoProvider(stub, name, id, balance) {
+        let newAdProvider={
+            providerName: name,
+            providerId: id,
+            balance: balance
+        }
+        await stub.putState('videoProvider'+ newAdProvider.providerId,
+                                  Buffer.from(JSON.stringify(newAdProvider)));
+        console.log('videoProvider '+newVideoProvider.providerId+' has already been added with balance '+newVideoProvider.balance);
     }
 
 
@@ -76,7 +130,7 @@ var Chaincode = class {
             return shim.success(payload);
         }
         catch(err){
-            consolo.log(err);
+            console.log(err);
             return shim.error(err);
         }
     }
@@ -86,23 +140,13 @@ var Chaincode = class {
         let tokenIssuer={
             balance: "10000"
         };
-        /*
-        let adAdProvider=[];
-        let video=[];
-        let videoProvider=[];
-        */
-
-        await stub.putState('tokenIssure',Buffer.from(JSON.stringify(tokenIssuer)));
-        console.info("tokenIssure has already been generated with balance: ", tokenIssuer.balance);
+        await stub.putState('tokenIssuer',Buffer.from(JSON.stringify(tokenIssuer)));
+        console.info("tokenIssuer has already been generated with balance: ", tokenIssuer.balance);
         
     }
 
 
 }
-
-
-
-
 
 
 
